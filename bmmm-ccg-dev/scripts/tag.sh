a316b78014fa19f2224f580756a32474b8099f5b
guido0013 <guido0013@hotmail.com>
2017-02-27 14:14:40 +0100

    update folders

--- bmmm-ccg-dev/scripts/tag.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Get the current version
-VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v INFO`
-
-java -jar target/bmmm-${VERSION}.jar $@
--- bmmm-ccg-dev/src/main/java/tagInducer/features/Features.java
+++
@@ -1,9 +0,0 @@
-package tagInducer.features;
-
-/**
- * Created by christos on 1/7/15.
- */
-public interface Features {
-
-    public int[][] getFeatures();
-}
